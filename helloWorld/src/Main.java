fa69824f8215711d848212a1164fa6877d41a064
George Panaretos <georgepanaretos.cac@gmail.com>
2018-12-16 19:22:54 +0100

    variables

--- helloWorld/src/Main.java
+++ helloWorld/src/Main.java
@@ -2,6 +2,32 @@ public class Main {
 
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        // int myFirstNumber =5;
+        int firstNumber = new Integer(5);
+        // System.out.println(myFirstNumber);
+        String firstString = "xs";
+        String secondString = new String("xs");
+        StringBuilder thirdString = new StringBuilder("xs");
+
+        if (firstString == secondString)
+            System.out.println("true");
+        else
+            System.out.println("false");
+//        if (thirdString == firstString)
+//            System.out.println("true");
+//        else
+//            System.out.println("false");
+
+
+        int myFirstNumber = (10 + 5) + (2 * 10);
+        int mySecondNumber = 12;
+        int myThirdNumber = myFirstNumber * 2;
+
+        int myTotal = myFirstNumber + mySecondNumber + myThirdNumber;
+
+        int myLastOne = 1000 - myTotal;
+
+        System.out.println(myLastOne);
 
     }
 }
